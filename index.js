b85f00c4479e8e4a6bb4934771986a84c29ba868
JSONRete <jsonrete@gmail.com>
2021-07-27 07:35:29 -0400

    commit tree 7ec5e8d9653007c8f21e4d993a6e13c814ddc576

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  coolText()
+  console.log("The DOM has loaded");
+
+});
+
+function coolText() {
+  document.getElementById('text')
+    .innerHTML = "This is really cool!"
+}
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
+
+
